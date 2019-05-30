a95098800d87a87e772cf46c15cace2c72dfdc3b
benevolent-listener-0694 <pranas_29@yahoo.co.uk>
2019-05-30 16:17:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(mika){
   return  `Happy holidays, ${'you'}!`
 }
 function happyHolidayTo(holiday, pranas){
-  return `Happy, ${'Happy Independence Day, you!'}, ${'You'}!`
+  return `Happy, ${'Happy Independence Day'}, ${'You'}!`
 }
 function holidayCountdown(days , sevendays){
   return `It's ${"It's 20 days until Mother's Day!"} days until${"Mother's Day", 20}!`
