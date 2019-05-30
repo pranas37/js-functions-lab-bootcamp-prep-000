a1ccf8a8392a1c628e2e611b6692ce9cd79d68a8
benevolent-listener-0694 <pranas_29@yahoo.co.uk>
2019-05-30 17:07:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(holiday, pranas){
   return `Happy ${holiday}, ${pranas}!`
 }
 function holidayCountdown(days , sevendays){
-  return `It's ${days} days until!${sevendays}!`
+  return `It's ${days} days until!${holiday}!`
 }
