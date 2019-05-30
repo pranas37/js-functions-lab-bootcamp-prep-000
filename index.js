ff88675a3a45906a65b2d174829ea32ef45b5411
benevolent-listener-0694 <pranas_29@yahoo.co.uk>
2019-05-30 15:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHollidayTo(holiday, pranas){
   return `Happy, ${'Independence Day'}, ${'you'}!`
 }
 function holidayCountdown(days , sevendays){
-  return `It's ${'It\'s 20 days until Mother\'s Day!'} days until${}!`
+  return `It's ${'It\'s 20 days until Mother\'s Day!'} days until${'It\'s It\'s 20, It\'s 20, Mother\'s day!! days untilIt\'s 20 days until Mother\'s Day!!'}!`
 }
